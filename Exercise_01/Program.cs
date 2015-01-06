72739886e9502c883e04a2f74c50064dd5d8ab29
winstonmuller <winstonmuller@gmail.com>
2015-01-06 21:35:05 +1300

    01 Number Formatted as String

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a method takes an integer and outputs it in the following format withing using String.Format 
+            //  10000000 = 10,000,000
+            //  Shows type conversion between collections.
+            StringBuilder result = new StringBuilder();
+
+            int num = 10000000;
+            var reversed = Reverse(num.ToString());
+
+            int cnt = 0;
+
+            foreach(char val in reversed)
+            {
+                if (cnt < 3)
+                {
+                    cnt++;
+                }
+                else
+                {
+                    result.Append(',');
+                    cnt = 1;
+                }
+                result.Append(val);
+            }
+
+            string straight = Reverse(result.ToString());
+            Console.WriteLine(straight);
+            Console.ReadLine();
+        }
+
+        public static string Reverse(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            char[] straight = input.ToCharArray();
+            ArrayList reverser = new ArrayList(straight);
+            reverser.Reverse();
+
+            foreach(var item in reverser)
+            {
+                result.Append(item);
+            }
+
+            return result.ToString();
+        }
+    }
+}
